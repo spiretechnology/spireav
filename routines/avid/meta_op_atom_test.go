b106be8a0958e85441682ada69daa428237de667
connerdouglass <conner.douglass@gmail.com>
2022-04-09 00:42:15 -0700

    Added unit test for Avid metadata

--- routines/avid/meta_op_atom_test.go
+++ routines/avid/meta_op_atom_test.go
@@ -1,35 +1,97 @@
 package avid_test
 
 import (
+	"context"
+	"path/filepath"
 	"testing"
+
+	"github.com/spiretechnology/spireav"
+	"github.com/spiretechnology/spireav/routines/avid"
+	"github.com/stretchr/testify/assert"
 )
 
-const avidProxyFilename = "../../reference-media/SS0815KB.01/SS0815KBV01.D97B84568C1FF8V.mxf"
+type testCase struct {
+	filename string
+	meta     avid.AvidMxfMeta
+}
+
+var testCases = []testCase{
+	{
+		filename: "../../test-files/avid/V01.DE767F09_179DE179DE134V.mxf",
+		meta: avid.AvidMxfMeta{
+			FilePackageUmid:      "0x060A2B340101010501010F1013000000179DE1349678990527A6147DDAC0824E",
+			ReelName:             "output.mp4",
+			ReelUmid:             "0x060A2B340101010501010F10130000001780D0799678990545D7147DDAC0824E",
+			MaterialPackageName:  "output.new.01",
+			MaterialPackageUmid:  "0x060A2B340101010501010F1013000000179DE12C96789905C680147DDAC0824E",
+			MediaComposerVersion: "Avid Media Composer 21.12.0.55552",
+			Timecode:             "00:00:00:00",
+			EssenceStream: &spireav.StreamMeta{
+				Index:     0,
+				CodecType: "video",
+			},
+		},
+	},
+	{
+		filename: "../../test-files/avid/A01.DE767F0A_179DE179DE147A.mxf",
+		meta: avid.AvidMxfMeta{
+			FilePackageUmid:     "0x060A2B340101010501010F1013000000179DE14796789905971C147DDAC0824E",
+			ReelName:            "output.mp4",
+			ReelUmid:            "0x060A2B340101010501010F10130000001780D0799678990545D7147DDAC0824E",
+			MaterialPackageName: "output.new.01",
+			MaterialPackageUmid: "0x060A2B340101010501010F1013000000179DE12C96789905C680147DDAC0824E",
+			// MediaComposerVersion: "Avid Media Composer 21.12.0.55552",
+			Timecode: "00:00:00:00",
+			EssenceStream: &spireav.StreamMeta{
+				Index:     1,
+				CodecType: "audio",
+			},
+		},
+	},
+	{
+		filename: "../../test-files/avid/A02.DE767F0B_179DE179DE157A.mxf",
+		meta: avid.AvidMxfMeta{
+			FilePackageUmid:     "0x060A2B340101010501010F1013000000179DE15796789905004D147DDAC0824E",
+			ReelName:            "output.mp4",
+			ReelUmid:            "0x060A2B340101010501010F10130000001780D0799678990545D7147DDAC0824E",
+			MaterialPackageName: "output.new.01",
+			MaterialPackageUmid: "0x060A2B340101010501010F1013000000179DE12C96789905C680147DDAC0824E",
+			// MediaComposerVersion: "Avid Media Composer 21.12.0.55552",
+			Timecode: "00:00:00:00",
+			EssenceStream: &spireav.StreamMeta{
+				Index:     2,
+				CodecType: "audio",
+			},
+		},
+	},
+}
 
 func TestParseAvidMxfOpAtomMeta(t *testing.T) {
 
-	// // Get the metadata for the file
-	// metaRaw, err := spireav.GetMetadata(context.Background(), avidProxyFilename)
-	// if err != nil {
-	// 	t.Fatal("Failed to get meta: ", err)
-	// }
+	for _, tc := range testCases {
 
-	// // Parse the Avid-specific metadata for the file
-	// meta, err := avid.ParseAvidMxfOpAtomMeta(metaRaw)
-	// if err != nil {
-	// 	t.Fatal("Failed to parse AVID meta: ", err)
-	// }
+		// Get the metadata for the file
+		metaRaw, err := spireav.GetMetadata(context.Background(), tc.filename)
+		if err != nil {
+			t.Fatal("Failed to get meta: ", err)
+		}
 
-	// log.Printf("%+v", meta)
+		// Parse the Avid-specific metadata for the file
+		meta, err := avid.ParseAvidMxfOpAtomMeta(metaRaw)
+		if err != nil {
+			t.Fatal("Failed to parse AVID meta: ", err)
+		}
 
-	// // Check the metadata
-	// assert.Equal(t, "0x060A2B340101010501010F1013000000568C1FF887119905F515003EE1CE9497", meta.FilePackageUmid)
-	// assert.Equal(t, "SS0815KB", meta.ReelName)
-	// assert.Equal(t, "0x060A2B340101010501010F101300000055455298871199055DAD003EE1CE9497", meta.ReelUmid)
-	// assert.Equal(t, "SS0815KB.06.new.01", meta.MaterialPackageName)
-	// assert.Equal(t, "0x060A2B340101010501010F1013000000568C1FF187119905A7C0003EE1CE9497", meta.MaterialPackageUmid)
-	// assert.Equal(t, "Avid Media Composer 18.12.5.51845", meta.MediaComposerVersion)
-	// assert.Equal(t, "15:21:37:14", meta.Timecode)
-	// assert.NotNil(t, meta.EssenceStream)
+		assert.Equal(t, tc.meta.FilePackageUmid, meta.FilePackageUmid, "file package umid: %s", filepath.Base(tc.filename))
+		assert.Equal(t, tc.meta.ReelName, meta.ReelName, "reel name: %s", filepath.Base(tc.filename))
+		assert.Equal(t, tc.meta.ReelUmid, meta.ReelUmid, "reel umid: %s", filepath.Base(tc.filename))
+		assert.Equal(t, tc.meta.MaterialPackageName, meta.MaterialPackageName, "material package name: %s", filepath.Base(tc.filename))
+		assert.Equal(t, tc.meta.MaterialPackageUmid, meta.MaterialPackageUmid, "material package umid: %s", filepath.Base(tc.filename))
+		assert.Equal(t, tc.meta.MediaComposerVersion, meta.MediaComposerVersion, "media composer version: %s", filepath.Base(tc.filename))
+		assert.Equal(t, tc.meta.Timecode, meta.Timecode, "timecode: %s", filepath.Base(tc.filename))
+		assert.NotNil(t, meta.EssenceStream, "essence stream nil: %s", filepath.Base(tc.filename))
+		assert.Equal(t, tc.meta.EssenceStream.Index, meta.EssenceStream.Index, "essence stream index: %s", filepath.Base(tc.filename))
+		assert.Equal(t, tc.meta.EssenceStream.CodecType, meta.EssenceStream.CodecType, "essence stream codec: %s", filepath.Base(tc.filename))
+	}
 
 }
