ce5d47f9a98fc3cd3879fa6ef4818fc40f783c95
connerdouglass <conner.douglass@gmail.com>
2023-06-29 08:37:52 -0400

    Better handling for temp dir in GHA test run

--- routines/avid/proxy_mp4_remuxer_test.go
+++ routines/avid/proxy_mp4_remuxer_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/spiretechnology/spireav"
@@ -25,7 +26,13 @@ func TestAvidRemux(t *testing.T) {
 	}
 
 	// Create the output directory
-	tempDir, err := os.MkdirTemp("", "*")
+	var tempDir string
+	if envTempDir, ok := os.LookupEnv("TEMP_DIR"); ok {
+		tempDir = filepath.Join(envTempDir, "spireav-remux-test")
+	} else {
+		tempDir = filepath.Join(os.TempDir(), "spireav-remux-test")
+	}
+	err = os.MkdirAll(tempDir, 0700)
 	assert.NoError(t, err, "create temp output dir")
 	defer os.RemoveAll(tempDir)
 
